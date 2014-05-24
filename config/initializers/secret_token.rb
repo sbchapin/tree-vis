3301e402c9be35f40cb9d0f5b0f8f5c8e839ae57
Sam Chapin <sam@hgdata.com>
2014-05-24 00:39:08 -0700

    New Rails project + Gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TreeVis::Application.config.secret_key_base = 'd4a1a773741a4644c332ac7e8feadfcfcd07f960d351c9d398f025ebdc6ea31dc0e1b8e50b67d16f78eb0da26d74d22c0f31d5b2d11a565625e832d142ec5127'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TreeVis::Application.config.session_store :cookie_store, key: '_tree-vis_session'
