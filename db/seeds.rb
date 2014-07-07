17b0e71af2ca7cea4713227254539d6741b31920
Sam Chapin <sam@hgdata.com>
2014-07-07 03:43:49 -0700

    Sample seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+antimass = Entity.find_or_create_by name: "Antimass Deflectors", description: "Your deflectors can't repel flavor of THAT magnitude!"
+cybernetics = Entity.find_or_create_by name: "Cybernetics", description: "Cyber me harder"
+xrds = Entity.find_or_create_by name: "XRDS Transporters", description: "Xeno-Reductive Demi-Snarfle"
+
+Relationship.find_or_create_by entity_source: antimass, entity_target: xrds
+Relationship.find_or_create_by entity_source: antimass, entity_target: cybernetics
